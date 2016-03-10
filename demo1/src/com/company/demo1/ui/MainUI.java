1c015d65f01f1ecbb8c7be0b25fd07b5cf7c636f
FHaerteis <FHaerteis@Bremen.XdevSoftware.local>
2016-03-10 16:11:22 +0100

    Init

---
+++ demo1/src/com/company/demo1/ui/MainUI.java
@@ -0,0 +1,40 @@
+
+package com.company.demo1.ui;
+
+import com.vaadin.annotations.Push;
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.xdev.ui.XdevUI;
+import com.xdev.ui.navigation.XdevNavigator;
+
+@Push
+@Theme("demo1")
+public class MainUI extends XdevUI {
+	public MainUI() {
+		super();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void init(VaadinRequest request) {
+		this.initUI();
+	}
+
+	/*
+	 * WARNING: Do NOT edit!<br>The content of this method is always regenerated
+	 * by the UI designer.
+	 */
+	// <generated-code name="initUI">
+	private void initUI() {
+		this.navigator = new XdevNavigator(this, this);
+
+		this.navigator.addView("", MainView.class);
+
+		this.setSizeFull();
+	} // </generated-code>
+
+	// <generated-code name="variables">
+	private XdevNavigator navigator; // </generated-code>
+}
