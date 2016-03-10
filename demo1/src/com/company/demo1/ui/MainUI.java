830b34022e7f9909fa8e06d4c56a9036c0d67182
FHaerteis <FHaerteis@Bremen.XdevSoftware.local>
2016-03-10 16:15:36 +0100

    test

--- demo1/src/com/company/demo1/ui/MainUI.java
+++ demo1/src/com/company/demo1/ui/MainUI.java
@@ -4,6 +4,7 @@ package com.company.demo1.ui;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.xdev.ui.XdevButton;
 import com.xdev.ui.XdevUI;
 import com.xdev.ui.navigation.XdevNavigator;
 
@@ -28,13 +29,19 @@ public class MainUI extends XdevUI {
 	 */
 	// <generated-code name="initUI">
 	private void initUI() {
+		this.button = new XdevButton();
 		this.navigator = new XdevNavigator(this, this);
-
+	
+		this.button.setCaption("Button");
 		this.navigator.addView("", MainView.class);
-
+	
+		this.button.setSizeUndefined();
+		this.setContent(this.button);
 		this.setSizeFull();
 	} // </generated-code>
 
 	// <generated-code name="variables">
-	private XdevNavigator navigator; // </generated-code>
+	private XdevNavigator navigator;
+	private XdevButton button; // </generated-code>
+	
 }
