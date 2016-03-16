035530ade00adcf860fc6777640bc42c39319ac2
Messe <Messe@Horus>
2016-03-16 14:02:48 +0100

    test

--- demo1/src/com/company/demo1/ui/MainView.java
+++ demo1/src/com/company/demo1/ui/MainView.java
@@ -30,6 +30,19 @@ public class MainView extends XdevView {
 		Notification.show("XDEV", "Hallo Richard!", Type.ERROR_MESSAGE);
 	}
 
+	
+
+	/**
+	 * Event handler delegate method for the {@link XdevButton}
+	 * {@link #button3}.
+	 *
+	 * @see Button.ClickListener#buttonClick(Button.ClickEvent)
+	 * @eventHandlerDelegate
+	 */
+	private void button3_buttonClick(Button.ClickEvent event) {
+		Notification.show("XDEV", "Hallo Welt!", Type.ERROR_MESSAGE);
+	}
+
 	/*
 	 * WARNING: Do NOT edit!<br>The content of this method is always regenerated
 	 * by the UI designer.
@@ -39,32 +52,40 @@ public class MainView extends XdevView {
 		this.gridLayout = new XdevGridLayout();
 		this.button = new XdevButton();
 		this.button2 = new XdevButton();
+		this.button3 = new XdevButton();
 	
+		this.button.setCaption("Horst");
 		this.button2.setCaption("Cool");
+		this.button3.setCaption("Button");
 	
-		this.gridLayout.setColumns(3);
-		this.gridLayout.setRows(3);
+		this.gridLayout.setColumns(4);
+		this.gridLayout.setRows(4);
 		this.button.setSizeUndefined();
 		this.gridLayout.addComponent(this.button, 0, 0);
 		this.button2.setSizeUndefined();
 		this.gridLayout.addComponent(this.button2, 1, 1);
+		this.button3.setSizeUndefined();
+		this.gridLayout.addComponent(this.button3, 2, 2);
 		CustomComponent gridLayout_hSpacer = new CustomComponent();
 		gridLayout_hSpacer.setSizeFull();
-		this.gridLayout.addComponent(gridLayout_hSpacer, 2, 0, 2, 1);
-		this.gridLayout.setColumnExpandRatio(2, 1.0F);
+		this.gridLayout.addComponent(gridLayout_hSpacer, 3, 0, 3, 2);
+		this.gridLayout.setColumnExpandRatio(3, 1.0F);
 		CustomComponent gridLayout_vSpacer = new CustomComponent();
 		gridLayout_vSpacer.setSizeFull();
-		this.gridLayout.addComponent(gridLayout_vSpacer, 0, 2, 1, 2);
-		this.gridLayout.setRowExpandRatio(2, 1.0F);
+		this.gridLayout.addComponent(gridLayout_vSpacer, 0, 3, 2, 3);
+		this.gridLayout.setRowExpandRatio(3, 1.0F);
 		this.gridLayout.setSizeFull();
 		this.setContent(this.gridLayout);
 		this.setSizeFull();
 	
 		button2.addClickListener(event -> this.button2_buttonClick(event));
+		button3.addClickListener(event -> this.button3_buttonClick(event));
 	} // </generated-code>
 
+
+
 	// <generated-code name="variables">
-	private XdevButton button, button2;
+	private XdevButton button, button2, button3;
 	private XdevGridLayout gridLayout; // </generated-code>
 
 
